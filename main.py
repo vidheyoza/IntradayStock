781549fb8eef1d2566e97e70fb9981e1691c4eb7
Vidhey Oza <vidhey97@gmail.com>
2019-11-18 19:26:08 -0500

    Final updates on milestone

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def error(y_pred, y_true):
     return (y_pred - y_true) ** 2
 
 
-def plot_error(model_name, y_pred, y_true):
+def plot_error(model_name, y_pred, y_true, path):
     """
     Plot squared errors for each test data point
 
@@ -35,6 +35,7 @@ def plot_error(model_name, y_pred, y_true):
     e = error(y_pred, y_true)
     plt.plot(e)
     plt.title(model_name + ' Evaluation')
+    plt.savefig(path + model_name + '_errors')
     plt.show()
 
 
@@ -42,10 +43,10 @@ def plot_error(model_name, y_pred, y_true):
 # DATA EXTRACTION
 # ===============
 
-data_path = '/Users/vidhey/coding/Python/IntradayStock/data/'
+path = '/Users/vidhey/coding/Python/IntradayStock/'
 stock = 'AAPL'
 
-data = pd.read_csv(data_path + stock + '_180101_191111.csv')
+data = pd.read_csv(path + 'data/' + stock + '_180101_191111.csv')
 data.drop(['<TICKER>', '<PER>', '<DATE>', '<TIME>'], axis=1, inplace=True)
 
 data = data.head(1000)
@@ -68,6 +69,24 @@ data = pd.DataFrame({
 
 data = data_to_indicators(data, o, h, l, c, v, window=10)
 data = data[100:-100]
+
+minutes = 390
+day = 1
+plt.plot(data[c].values[day * minutes:(day + 1) * minutes])
+plt.title('Minute-wise closing price of AAPL for 2018-01-03')
+plt.savefig(path + 'plots/AAPL_20180103')
+plt.show()
+
+plt.plot(data[v].values[day * minutes:(day + 1) * minutes])
+plt.title('Minute-wise traded volume of AAPL for 2018-01-03')
+plt.savefig(path + 'plots/AAPL_vol_20180103')
+plt.show()
+
+plt.plot(data['macd_10'].values[day * minutes:(day + 1) * minutes])
+plt.plot(np.zeros(data['macd_10'].values[day * minutes:(day + 1) * minutes].shape), linestyle=':', c='black')
+plt.title('Minute-wise MACD of AAPL for 2018-01-03')
+plt.savefig(path + 'plots/AAPL_macd_20180103')
+plt.show()
 
 # ===============
 # DATA EXTRACTION
@@ -110,14 +129,32 @@ xgb_model.fit(X_train, y_train)
 # SUPPORT VECTOR REGRESSOR
 y_pred_svr = svr_model.predict(X_test)
 svr_mse = mean_squared_error(y_pred_svr, y_test)
-plot_error('SVR', y_pred_svr, y_test)
+plot_error('SVR', y_pred_svr, y_test, path=path + 'plots/')
+
+plt.plot(y_test, c='black', label='Real Price')
+plt.plot(y_pred_svr, c='blue', label='Predicted Price')
+plt.legend()
+plt.savefig(path + 'plots/SVR_pred')
+plt.show()
 
 # RANDOM FOREST
 y_pred_rf = rf_model.predict(X_test)
 rf_mse = mean_squared_error(y_pred_rf, y_test)
-plot_error('Random Forest', y_pred_rf, y_test)
+plot_error('RandomForest', y_pred_rf, y_test, path=path + 'plots/')
+
+plt.plot(y_test, c='black', label='Real Price')
+plt.plot(y_pred_rf, c='blue', label='Predicted Price')
+plt.legend()
+plt.savefig(path + 'plots/RF_pred')
+plt.show()
 
 # XGBOOST
 y_pred_xgb = xgb_model.predict(X_test)
 xgb_mse = mean_squared_error(y_pred_xgb, y_test)
-plot_error('XGBoost', y_pred_xgb, y_test)
+plot_error('XGBoost', y_pred_xgb, y_test, path=path + 'plots/')
+
+plt.plot(y_test, c='black', label='Real Price')
+plt.plot(y_pred_xgb, c='blue', label='Predicted Price')
+plt.legend()
+plt.savefig(path + 'plots/XGB_pred')
+plt.show()
